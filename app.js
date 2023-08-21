2ef8a25ce5c0850ae5b6d761c60d9c639ddc57d7
Anshita <nayakanshita27@gmail.com>
2023-08-21 20:04:17 +0530

    first commit

--- app.js
+++ app.js
@@ -12,27 +12,28 @@ const PORT = process.env.PORT;
 
 require('./db/conn');
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-app.use(cookieParser());
-app.use(express.json());
-app.use(require('./router/auth'));
-
-// app.use(express.static(path.join(__dirname, './client/build')));
-
-// app.get('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
-
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 app.get("/*", function (_, res) {
     res.sendFile(
         path.resolve(__dirname, 'client', 'build')
     );
 });
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(require('./router/auth'));
+
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build/index.html'));
+// });
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
